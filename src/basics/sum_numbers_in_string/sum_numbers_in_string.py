08b9f5adf942b70b2473189fbb8420fcafb499e5
evigo-leo <leonenkov_e.i@mail.ru>
2026-09-03 16:35:47 +0300

    Add function sum_numbers_in_string

--- src/basics/sum_numbers_in_string/sum_numbers_in_string.py
+++ src/basics/sum_numbers_in_string/sum_numbers_in_string.py
@@ -1,5 +1,6 @@
 import re
 
+
 def sum_numbers_in_string(input_string: str) -> int:
     """
     Находит все целые числа в строке и возвращает их сумму.
@@ -10,8 +11,20 @@ def sum_numbers_in_string(input_string: str) -> int:
     Returns:
         Сумма всех найденных целых чисел.
     """
-    # TODO: Реализуйте функцию
-    pass
+    numbers_list = []
+    number = ""
+    for letter in input_string:
+        if letter.isdigit() or letter in '.':
+            number += letter
+        else:
+            if number != "":
+                numbers_list.append(int(float(number)))
+                number = ""
+            else:
+                continue
+    if number != "":
+        numbers_list.append(int(float(number)))
+    return sum(numbers_list)
 
 def sum_numbers_in_string_regex(input_string: str) -> int:
     """
@@ -23,5 +36,38 @@ def sum_numbers_in_string_regex(input_string: str) -> int:
     Returns:
         Сумма всех найденных целых чисел.
     """
-    # TODO: Реализуйте функцию
-    pass
+    pattern = r'\d+(?:\.\d+)?'
+    str_list = re.findall(pattern, input_string)
+    numbers_list = [int(float(num)) for num in str_list]
+    return sum(numbers_list)
+
+
+# string_1 = "I have 10 apples and 5 oranges"
+# print(sum_numbers_in_string(string_1))
+
+# string_2 = "No numbers here"
+# print(sum_numbers_in_string(string_2))
+
+# string_3 = "123 and 456 make 579"
+# print(sum_numbers_in_string(string_3))
+
+# string_4 = "1a2b3c4d"
+# print(sum_numbers_in_string(string_4))
+
+# string_5 = "Price is $19.99 and $29.99"
+# print(sum_numbers_in_string(string_5))
+
+# string_1 = "I have 10 apples and 5 oranges"
+# print(sum_numbers_in_string_regex(string_1))
+
+# string_2 = "No numbers here"
+# print(sum_numbers_in_string_regex(string_2))
+
+# string_3 = "123 and 456 make 579"
+# print(sum_numbers_in_string_regex(string_3))
+
+# string_4 = "1a2b3c4d"
+# print(sum_numbers_in_string_regex(string_4))
+
+# string_5 = "Price is $19.99 and $29.99"
+# print(sum_numbers_in_string_regex(string_5))
