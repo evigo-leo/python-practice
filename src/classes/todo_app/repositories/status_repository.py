22b41752c9fe473ae3f6c3feacfa03c54aa8ca25
evigo-leo <leonenkov_e.i@mail.ru>
2026-09-18 02:03:45 +0300

    Add status repository module

--- src/classes/todo_app/repositories/status_repository.py
+++ src/classes/todo_app/repositories/status_repository.py
@@ -1,15 +1,30 @@
+"""
+Status repo module
+"""
 from src.classes.todo_app.models.status import Status
 from src.classes.todo_app.repositories.base_repository import BaseRepository
 
+
 class StatusRepository(BaseRepository[Status]):
+    """Class for status repo"""
     def __init__(self, file_path: str):
-        # TODO: Реализуйте инициализацию
-        pass
+        super().__init__(file_path=file_path, model=Status)
+        if not self._data:
+            self._init_default_statuses()
 
     def _init_default_statuses(self) -> None:
-        # TODO: Реализуйте инициализацию статусов по умолчанию
-        pass
+        """Initialize default statuses"""
+        default_statuses = [
+            Status(id=1, name="В ожидании"),
+            Status(id=2, name="В работе"),
+            Status(id=3, name="Завершено")
+        ]
+
+        for status in default_statuses:
+            self._data[status.id] = status
+
+        self._save()
 
     def is_valid_status(self, status_id: int) -> bool:
-        # TODO: Реализуйте проверку валидности статуса
-        pass
+        """Check a status valid"""
+        return status_id in self._data
