6f7e85403ef7c076efb4be8c2755b2a59ea46071
evigo-leo <leonenkov_e.i@mail.ru>
2026-09-18 02:05:00 +0300

    Add category repository module

--- src/classes/todo_app/repositories/category_repository.py
+++ src/classes/todo_app/repositories/category_repository.py
@@ -1,12 +1,17 @@
+"""Category Repository module"""
 from src.classes.todo_app.models.category import Category
 from src.classes.todo_app.repositories.base_repository import BaseRepository
-from typing import Optional
+
 
 class CategoryRepository(BaseRepository[Category]):
+    """The category class repo"""
     def __init__(self, file_path: str):
-        # TODO: Реализуйте инициализацию
-        pass
+        super().__init__(file_path=file_path, model=Category)
 
-    def get_by_name(self, name: str) -> Optional[Category]:
-        # TODO: Реализуйте поиск по имени
-        pass
+    def get_by_name(self, name: str) -> Category | None:
+        """Get category by a name"""
+        target_name = name.lower()
+        for category in self.get_all():
+            if category.name.lower() == target_name:
+                return category
+        return None
