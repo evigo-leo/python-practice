7c884c97cbe4850d87b67ca5b8c37973ba92a269
evigo-leo <leonenkov_e.i@mail.ru>
2026-09-18 16:02:10 +0300

    Add task repository module

--- src/classes/todo_app/repositories/task_repository.py
+++ src/classes/todo_app/repositories/task_repository.py
@@ -1,18 +1,25 @@
-from typing import List
+"""
+Task repo module
+"""
 from src.classes.todo_app.models.task import Task
 from src.classes.todo_app.repositories.base_repository import BaseRepository
 
 class TaskRepository(BaseRepository[Task]):
     def __init__(self, file_path: str):
-        # TODO: Реализуйте инициализацию
-        pass
+         super().__init__(file_path=file_path, model=Task)
 
-    def get_by_category(self, category_id: int) -> List[Task]:
+    def get_by_category(self, category_id: int) -> list[Task]:
         """Get all tasks belonging to a specific category."""
-        # TODO: Реализуйте получение задач по категории
-        pass
-
-    def get_by_status(self, status_id: int) -> List[Task]:
+        tasks_by_category = []
+        for task in self.get_all():
+            if task.category_id == category_id:
+                tasks_by_category.append(task)
+        return tasks_by_category
+        
+    def get_by_status(self, status_id: int) -> list[Task]:
         """Get all tasks with a specific status."""
-        # TODO: Реализуйте получение задач по статусу
-        pass
+        tasks_by_status = []
+        for task in self.get_all():
+            if task.status_id == status_id:
+                tasks_by_status.append(task)
+        return tasks_by_status
